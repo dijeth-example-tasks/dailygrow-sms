67b7b1cf68616b3b29e18335cd3f83380e270ea0
Dmitry Orlov <d.orlov777@gmail.com>
2024-03-08 16:15:59 +0300

    fix: middleware to response json

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -21,7 +21,7 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                return $request->wantsJson() ? response()->json(['error' => 'Already authentificated'], 200) : redirect(RouteServiceProvider::HOME);
             }
         }
 
