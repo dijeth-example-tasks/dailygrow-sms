b97793a9536eff493a55885019ee1740bdc2eebb
Dmitry Orlov <d.orlov777@gmail.com>
2024-03-09 18:30:44 +0300

    drop messages

--- app/Services/TaskRunnerService.php
+++ app/Services/TaskRunnerService.php
@@ -2,12 +2,9 @@
 
 namespace App\Services;
 
-use App\Models\Message;
 use App\Models\Task;
 use App\Models\TaskRun;
 use App\Services\Sms\SmsSender;
-use Carbon\CarbonImmutable;
-use Illuminate\Support\Collection;
 
 
 class TaskRunnerService
